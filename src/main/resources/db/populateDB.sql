830c26d9b54d7f392534e2023814ceaa73d71a81
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-06-28 13:25:40 +0300

    Populate dish in populateDB.sql

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM users;
+DELETE FROM dishes;
 DELETE FROM menus;
 DELETE FROM restaurants;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -20,3 +21,20 @@ INSERT INTO menus (registered, restaurant_id) VALUES ('2016-06-26 09:00:00', 100
 INSERT INTO menus (registered, restaurant_id) VALUES ('2016-06-27 09:00:00', 100002);
 
 INSERT INTO menus (registered, restaurant_id) VALUES ('2016-06-27 08:30:00', 100003);
+
+
+INSERT INTO dishes (name, price, menu_id) VALUES ('Борщ', 50, 100004);
+
+INSERT INTO dishes (name, price, menu_id) VALUES ('Пюре', 30, 100004);
+
+INSERT INTO dishes (name, price, menu_id) VALUES ('Котлета', 50, 100004);
+
+INSERT INTO dishes (name, price, menu_id) VALUES ('Салат', 40, 100004);
+
+INSERT INTO dishes (name, price, menu_id) VALUES ('Солянка', 70, 100005);
+
+INSERT INTO dishes (name, price, menu_id) VALUES ('Макароны', 20, 100005);
+
+INSERT INTO dishes (name, price, menu_id) VALUES ('Бифштекс', 60, 100005);
+
+INSERT INTO dishes (name, price, menu_id) VALUES ('Компот', 10, 100005);
