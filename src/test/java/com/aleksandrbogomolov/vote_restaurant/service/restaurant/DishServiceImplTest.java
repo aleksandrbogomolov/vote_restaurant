b0c22d2bd6e86d0a3f3aa22fb688186cec3b3c77
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-07-18 15:29:16 +0300

    Refactoring DishServiceImplTest

--- src/test/java/com/aleksandrbogomolov/vote_restaurant/service/restaurant/DishServiceImplTest.java
+++ src/test/java/com/aleksandrbogomolov/vote_restaurant/service/restaurant/DishServiceImplTest.java
@@ -2,8 +2,8 @@ package com.aleksandrbogomolov.vote_restaurant.service.restaurant;
 
 import com.aleksandrbogomolov.vote_restaurant.model.restaurant.Dish;
 import com.aleksandrbogomolov.vote_restaurant.service.AbstractServiceTest;
-import com.aleksandrbogomolov.vote_restaurant.test_data.DishTestData;
 import com.aleksandrbogomolov.vote_restaurant.util.exception.NotFoundException;
+import lombok.val;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,8 +20,8 @@ public class DishServiceImplTest extends AbstractServiceTest {
 
     @Test
     public void save() throws Exception {
-        DishTestData.TestDish testDish = new DishTestData.TestDish("New", 50, 1);
-        Dish dish = service.save(testDish.asDish(), 100002);
+        TestDish testDish = new TestDish("New", 50, 1);
+        val dish = service.save(testDish.asDish(), 100002);
         testDish.setId(dish.getId());
         testDish.setRestaurant(dish.getRestaurant());
         MATCHER.assertEquals(testDish, service.getOne(100013, 100002));
@@ -29,7 +29,7 @@ public class DishServiceImplTest extends AbstractServiceTest {
 
     @Test
     public void update() throws Exception {
-        DishTestData.TestDish updateDish = new DishTestData.TestDish(DISH_1);
+        TestDish updateDish = new TestDish(DISH_1);
         updateDish.setName("Гороховый суп");
         service.update(updateDish.asDish(), 100002);
         MATCHER.assertEquals(updateDish, service.getOne(100004, 100002));
@@ -37,13 +37,13 @@ public class DishServiceImplTest extends AbstractServiceTest {
 
     @Test(expected = NotFoundException.class)
     public void notFoundUpdate() {
-        Dish dish = service.getOne(100004, 100002);
+        val dish = service.getOne(100004, 100002);
         service.update(dish, 100003);
     }
 
     @Test
     public void delete() throws Exception {
-        service.delete(100004,100002);
+        service.delete(100004, 100002);
         MATCHER.assertCollectionEquals(sortArrays().subList(1, sortArrays().size()), service.getAll(100002));
     }
 
@@ -68,7 +68,7 @@ public class DishServiceImplTest extends AbstractServiceTest {
     }
 
     private List<Dish> sortArrays() {
-        Dish[] dishes = new Dish[]{DISH_1, DISH_3, DISH_5, DISH_7};
+        val dishes = new Dish[]{DISH_1, DISH_3, DISH_5, DISH_7};
         Arrays.sort(dishes, (d1, d2) -> d1.getName().compareTo(d2.getName()));
         return Arrays.asList(dishes);
     }
