bcc8e964396a146e974406aa6db866906664c6e1
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-07-20 10:46:29 +0300

    Some change SpringWebConfig

--- src/main/java/com/aleksandrbogomolov/vote_restaurant/configuration/SpringWebConfiguration.java
+++ src/main/java/com/aleksandrbogomolov/vote_restaurant/configuration/SpringWebConfiguration.java
@@ -30,7 +30,7 @@ public class SpringWebConfiguration extends WebMvcConfigurerAdapter implements A
     @Bean
     public ResourceBundleMessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasenames(new String[]{"messages/header", "messages/user", "messages/footer", "messages/admin", "messages/profile", "messages/signin"});
+        messageSource.setBasenames("messages/header", "messages/user", "messages/footer", "messages/admin", "messages/profile", "messages/signin");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
