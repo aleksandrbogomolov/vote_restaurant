60b9617560828b594c76dfee1450d9a6687e2982
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-07-23 10:50:56 +0300

    Update Thymeleaf variables

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,8 +2,4 @@ $('.scroll-top').click(function () {
     $('body,html').animate({scrollTop: 0}, 1000);
 });
 
-// $('.dropdown-toggle').click(function () {
-//     $('.dropdown-menu').toggle();
-// });
-
 $('.collapse').collapse('toggle');
