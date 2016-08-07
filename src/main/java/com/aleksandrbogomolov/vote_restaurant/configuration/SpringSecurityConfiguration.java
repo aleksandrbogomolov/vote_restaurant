f640ebbe41a8ef8d40406cb89d9deeca7110ab4e
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-08-07 14:26:26 +0300

    Add AuthenticationSuccessHandler

--- src/main/java/com/aleksandrbogomolov/vote_restaurant/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/aleksandrbogomolov/vote_restaurant/configuration/SpringSecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.aleksandrbogomolov.vote_restaurant.configuration;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +10,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter implements AuthenticationSuccessHandler {
 
     private final UserDetailsService userService;
+
+    @Getter
+    @Setter
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
     @Autowired
     public SpringSecurityConfiguration(@Qualifier("userService") UserDetailsService userService) {
@@ -42,5 +61,38 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        handle(request, response, authentication);
+        clearAuthenticationAttributes(request);
+    }
+
+    private String determineTargetUrl(Authentication authentication) {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (GrantedAuthority authority : authorities) {
+            if (authority.getAuthority().equals("ROLE_ADMIN")) {
+                return "admin";
+            } else if (authority.getAuthority().equals("ROLE_USER")) {
+                return "restaurant";
+            } else {
+                throw new IllegalStateException();
+            }
+        }
+        return "login";
+    }
+
+    private void handle(HttpServletRequest req, HttpServletResponse res, Authentication authentication) throws IOException {
+        String targetUrl = determineTargetUrl(authentication);
+        redirectStrategy.sendRedirect(req, res, targetUrl);
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest req) {
+        HttpSession session = req.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
     }
 }
