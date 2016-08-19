3136833206321a0c4e740c6f3ac56fdb872f1361
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-08-19 14:20:06 +0300

    Change postgres.properties

--- src/main/java/com/aleksandrbogomolov/vote_restaurant/configuration/DataBaseConfiguration.java
+++ src/main/java/com/aleksandrbogomolov/vote_restaurant/configuration/DataBaseConfiguration.java
@@ -31,7 +31,7 @@ public class DataBaseConfiguration {
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
         properties.put("hibernate.use_sql_comments", environment.getRequiredProperty("hibernate.use_sql_comments"));
-//        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         return properties;
     }
 
